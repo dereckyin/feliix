58f169712fd23b76e4e32c644b08cd48c3e332ec
dereckyin <dereckyin>
2024-01-22 11:04:59 +0800

    Update transmittal_mgt.php

--- api/transmittal_mgt.php
+++ api/transmittal_mgt.php
@@ -139,14 +139,14 @@ if($fto != "")
 
 if($fdf != "")
 {
-    $query = $query . " and DATE_FORMAT(pm.transmittal_date, '%Y/%m/%d') >= '" . $fdf . "' ";
-    $query_cnt = $query_cnt . " and DATE_FORMAT(pm.transmittal_date, '%Y/%m/%d') >= '" . $fdf . "' ";
+    $query = $query . " and STR_TO_DATE(pm.transmittal_date, '%Y/%m/%d') >= '" . $fdf . "' ";
+    $query_cnt = $query_cnt . " and STR_TO_DATE(pm.transmittal_date, '%Y/%m/%d') >= '" . $fdf . "' ";
 }
 
 if($fdt != "")
 {
-    $query = $query . " and DATE_FORMAT(pm.transmittal_date, '%Y/%m/%d') <= '" . $fdt . "' ";
-    $query_cnt = $query_cnt . " and DATE_FORMAT(pm.transmittal_date, '%Y/%m/%d') <= '" . $fdt . "' ";
+    $query = $query . " and STR_TO_DATE(pm.transmittal_date, '%Y/%m/%d') <= '" . $fdt . "' ";
+    $query_cnt = $query_cnt . " and STR_TO_DATE(pm.transmittal_date, '%Y/%m/%d') <= '" . $fdt . "' ";
 }
 
 if($fpc != "")
