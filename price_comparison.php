f9416f4c5eef519066572dae593b73e64dc222ea
dereckyin <dereckyin>
2024-02-01 22:14:28 +0800

    Update price_comparison.php

--- price_comparison.php
+++ price_comparison.php
@@ -459,7 +459,7 @@ header( 'location:index' );
             text-align: left;
         }
 
-        #tb_specification_list tbody tr td:nth-of-type(3) div.code {
+        #tb_specification_list tbody tr td:nth-of-type(3) div.pid, #tb_specification_list tbody tr td:nth-of-type(3) div.code {
             font-size: 16px;
             font-weight: 800;
             word-break: break-all;
@@ -920,7 +920,7 @@ header( 'location:index' );
             max-width: 120px;
         }
 
-        .tb_format1 tbody tr td div.code {
+        .tb_format1 tbody tr td div.pid, .tb_format1 tbody tr td div.code {
             font-size: 16px;
             font-weight: 800;
             word-break: break-all;
@@ -3190,6 +3190,7 @@ header( 'location:index' );
                                                     <img v-if="row.gp1.url1 !==''" :src="row.gp1.url1">
                                                 </td>
                                                 <td rowspan="2" class="spec">
+                                                    <div class="pid noPrint" v-if="row.gp1.pid != 0">{{ "ID: " + row.gp1.pid }}</div>
                                                     <div class="code" v-if="row.gp1.code != ''">{{ row.gp1.code }}</div>
                                                     <div class="brief" style="white-space: pre-line;" v-if="row.gp1.desc != ''">{{ row.gp1.desc }}
                                                     </div>
@@ -3219,6 +3220,7 @@ header( 'location:index' );
                                                     <img v-if="row.gp2.url1 !==''" :src="row.gp2.url1">
                                                 </td>
                                                 <td rowspan="2" class="spec">
+                                                    <div class="pid noPrint" v-if="row.gp2.pid != 0">{{ "ID: " + row.gp2.pid }}</div>
                                                     <div class="code" v-if="row.gp2.code != ''">{{ row.gp2.code }}</div>
                                                     <div class="brief" style="white-space: pre-line;" v-if="row.gp2.desc != ''">{{ row.gp2.desc }}
                                                     </div>
@@ -3249,6 +3251,7 @@ header( 'location:index' );
                                                     <img v-if="org_options.length == 3 && row.gp3.url1 !==''" :src="row.gp3.url1">
                                                 </td>
                                                 <td rowspan="2" class="spec" v-if="org_options.length == 3">
+                                                    <div class="pid noPrint" v-if="row.gp3.pid != 0">{{ "ID: " + row.gp3.pid }}</div>
                                                     <div class="code" v-if="org_options.length == 3 && row.gp3.code != ''">{{ row.gp3.code }}</div>
                                                     <div class="brief" style="white-space: pre-line;" v-if="org_options.length == 3 && row.gp3.desc != ''">{{ row.gp3.desc }}
                                                     </div>
