bff3c1047733617f85d10946a0c162c1a87152d0
dereckyin <dereckyin@yahoo.com.tw>
2024-09-10 08:57:57 +0800

    Update mail.php

--- api/mail.php
+++ api/mail.php
@@ -11301,7 +11301,7 @@ function order_notification02($name, $access,  $access_cc, $project_name, $seria
         //     }
         // }
         
-        $receiver = rtrim($receiver, ", ");
+        $receiver = "All";
 
         $mail->Subject = 'Testing info for items of "' . $order_type . ': ' . $serial_name . '" is updated';
         $header = $name . ' updated the testing info for items of "' . $order_type . ': ' . $serial_name . '". Please check details below:';
@@ -11828,7 +11828,8 @@ function mockup_notification02($name, $access,  $access_cc, $project_name, $seri
             }
         }
         
-        $receiver = rtrim($receiver, ", ");
+        // $receiver = rtrim($receiver, ", ");
+        $receiver = "All";
 
         $mail->Subject = 'Testing info for items of "' . $order_type . ': ' . $serial_name . '" is updated';
         $header = $name . ' updated the testing info for items of "' . $order_type . ': ' . $serial_name . '". Please check details below:';
