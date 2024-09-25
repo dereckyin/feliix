75168aded0d26c48a6f5970fe7445b543d509a75
dereckyin <dereckyin>
2024-09-25 15:43:28 +0800

    test

--- quotation_pageless.php
+++ quotation_pageless.php
@@ -2593,7 +2593,7 @@ header( 'location:index' );
                 </div>
 
                 <div class="popupblock">
-                    <a id="" class="print" title="Export Whole Quotation into PDF" onclick="generate_pdf_test()"></a>
+                    <a id="" class="print" title="Export Whole Quotation into PDF" href="http://feliixload.myvnc.com/quotation_pageless_test?id=<?php echo $id; ?>"></a>
                 </div>
 
                 <div class="popupblock">
