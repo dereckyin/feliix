8747f16a94832ae2d2fd4d17a21e9ee9b25d8ac1
dereckyin <dereckyin>
2025-03-06 13:19:27 +0800

    fix 01

--- js/apply_for_leave_v2.js
+++ js/apply_for_leave_v2.js
@@ -268,7 +268,7 @@ let _this = this;
           this.apply_end = this.normalize(this.apply_end);
 
 
-          if(this.leave_level == 'B' || this.leave_level == 'C' || this.leave_type == 'H')
+          if(this.leave_level == 'B' || this.leave_level == 'C' || this.leave_type == 'H' || this.leave_type == 'U')
           {
             var timeStart = this.apply_start.slice(0, 10);
 
@@ -511,7 +511,7 @@ let _this = this;
     var amEnd = 'P';
 
 
-    if(this.leave_level == 'B' || this.leave_level == 'C' || this.leave_type == 'H')
+    if(this.leave_level == 'B' || this.leave_level == 'C' || this.leave_type == 'H' || this.leave_type == 'U')
     {
       var timeStart = this.apply_start.slice(0, 10);
 
@@ -580,7 +580,7 @@ let _this = this;
     var d_manager =  new Date(today.getFullYear(), "11", 0);
     var d_next_year =  new Date(today.getFullYear() + 1, 10, 0);
 
-    if(this.leave_level == 'B' || this.leave_level == 'C' || this.leave_type == 'H')
+    if(this.leave_level == 'B' || this.leave_level == 'C' || this.leave_type == 'H' || this.leave_type == 'U')
     {
       if(d1 > d_manager)
       {
