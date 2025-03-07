921054ba8e719e50820cb1acab50c43f2a0a9a2c
dereckyin <dereckyin>
2025-03-07 09:06:59 +0800

    Update user_profile.php

--- admin/user_profile.php
+++ admin/user_profile.php
@@ -170,12 +170,7 @@ catch (Exception $e) {
     <div class="mainContent" id="mainContent">
         <!-- tags js在 main.js -->
         <div class="tags">
-            <a class="tag A" href="user">User</a>
             <a class="tag F focus">User Profile</a>
-            <a class="tag B" href="department">Department</a>
-            <a class="tag C" href="position">Position</a>
-            <a class="tag D" href="leave_flow">Leave Flow</a>
-            <a class="tag E" href="expense_flow">Expense Flow</a>
         </div>
         <!-- Blocks -->
         <div class="block F focus">
