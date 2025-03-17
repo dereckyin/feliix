d38e556f50b669e408d680c4e7e3fd39ccffcefd
dereckyin <dereckyin>
2025-03-17 16:28:57 +0800

    fix dio to dios

--- api/sales_dashboard.php
+++ api/sales_dashboard.php
@@ -36,7 +36,7 @@ if (!isset($jwt)) {
             $access7 = true;
         }
        
-        if($username == "Kristel Tan" || $username == "Kuan" || $username == "Dennis Lin" || $username == "Marie Kayla Patricia Dequina" || $username == "Gina Donato" || $username == "dereck" || $username == "Aiza Eisma" || $username == "Johmar Maximo" || $username == "Stephanie De dio")
+        if($username == "Kristel Tan" || $username == "Kuan" || $username == "Dennis Lin" || $username == "Marie Kayla Patricia Dequina" || $username == "Gina Donato" || $username == "dereck" || $username == "Aiza Eisma" || $username == "Johmar Maximo" || $username == "Stephanie De dios")
         {
             $access7 = true;
         }
